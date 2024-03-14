39116977c6a17a9468a7a572a1cef762782b3813
Etwashoeren <sungmin0609@nate.com>
2024-03-14 16:27:59 +0900

    스프링부트3 백엔드 개발자되기

---
+++ springboot-developer/src/main/java/org/example/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,46 @@
+package org.example.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootdeveloper.domain.Article;
+import org.example.springbootdeveloper.dto.AddArticleRequest;
+import org.example.springbootdeveloper.dto.UpdateArticleRequest;
+import org.example.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service // 빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) { // ID를 받아 엔티티를 조회하고 없으면 IllelgalArgumentException 예외 발생 처리
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional // 트랜잭션 메서드
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+}
